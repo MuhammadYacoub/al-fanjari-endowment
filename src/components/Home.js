15c8ec7ca3425acedf51d1a5ac1de202f63acc32
MuhammadYacoub <ya3qoup@gmail.com>
2024-08-06 12:43:53 +0300

    images

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import profilePicture from '../assets/profile-picture.jpg'; // تأكد من أن مسار الصورة صحيح
 import filler from '../assets/filler.jpg';
+import filler2 from '../assets/filler2.jpg';
+import filler3 from '../assets/filler3.jpg';
+import filler4 from '../assets/filler4.jpg';
+
+
 
 
 
@@ -20,35 +25,35 @@ const Home = () => {
           <Card className="mb-4 w-100 p-0">
             <Card.Body>
             <img src={filler} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
-            <Card.Title className='card-header' > مسابقات الوقف </Card.Title>
-              <Card.Text>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
+            <Card.Title className='card-header bg-info text-white' > مسابقات الوقف </Card.Title>
+              <Card.Text className='card-body'>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={3}>
           <Card className="mb-4">
             <Card.Body>
-            <img src={filler} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
-              <Card.Title>المنح الدراسية  </Card.Title>
-              <Card.Text>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
+            <img src={filler2} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
+              <Card.Title className='card-header bg-info text-white'>المنح الدراسية  </Card.Title>
+              <Card.Text className='card-body'>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={3}>
           <Card className="mb-4">
             <Card.Body>
-            <img src={filler} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
-              <Card.Title> المكتبة </Card.Title>
-              <Card.Text>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
+            <img src={filler3} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
+              <Card.Title className='card-header bg-info text-white'> المكتبة </Card.Title>
+              <Card.Text className='card-body'>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={3}>
           <Card className="mb-4">
             <Card.Body>
-            <img src={filler} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
-              <Card.Title> نظارة الوقف </Card.Title>
-              <Card.Text>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
+            <img src={filler4} alt="Profile" className="rounded-circle mb-3" width="100%" height="auto" />
+              <Card.Title className='card-header bg-info text-white'> نظارة الوقف </Card.Title>
+              <Card.Text className='card-body'>وقف المرحوم المستشار الدكتور محمد شوقي الفنجري لصالح خدمة الدعوة والفقه الإسلامي برئاسة المستشار مسعد عبد المقصود الفخراني</Card.Text>
             </Card.Body>
           </Card>
         </Col>
